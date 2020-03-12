45da4e2957ea817ab02363b9167d5e5d9b2e9b2f
Anthony Li <li.anthony917@gmail.com>
2020-03-12 17:11:44 -0400

    Add Next

--- src/server.js
+++ src/server.js
@@ -7,8 +7,11 @@ const { ApolloServer, gql } = require("apollo-server-koa");
 const { Pool } = require("pg");
 const resolvers = require("./resolvers");
 const authRoutes = require("./auth/routes");
+const next = require("next");
 
 const typedefs = fs.readFileSync(path.join(__dirname, "types.graphql"), "utf8");
+
+const port = process.env.PORT || 3000;
 
 const db = new Pool({
     connectionString: process.env.DATABASE_URL
@@ -23,6 +26,12 @@ const authRouter = authRoutes(db, "/api/auth");
 app.use(authRouter.routes());
 app.use(authRouter.allowedMethods());
 
-app.listen({port: 3000}, () => {
-    console.log(`Server ready: https://localhost:3000${server.graphqlPath}`);
+const nextApp = next({dev: process.env.NODE_ENV !== "production"});
+const handle = nextApp.getRequestHandler();
+
+nextApp.prepare().then(() => {
+    app.use(ctx => handle(ctx.req, ctx.res));
+    app.listen({port}, () => {
+        console.log(`Server ready: https://localhost:${port}`);
+    });
 });
